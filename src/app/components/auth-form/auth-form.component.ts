4d9a72cf1eb63cf081efab87a399e7844115f996
vanhoa690 <vanhoa690@gmail.com>
2024-09-20 09:57:44 +0700

    validate form

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { User } from '../../services/auth.service';
 
 @Component({
@@ -13,12 +18,12 @@ export class AuthFormComponent {
   @Output() onSubmit = new EventEmitter<User>();
 
   authForm: FormGroup = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
   });
 
   handleSubmit() {
-    console.log(this.authForm.value);
-    this.onSubmit.emit(this.authForm.value);
+    console.log(this.authForm.get('email'));
+    // this.onSubmit.emit(this.authForm.value);
   }
 }
